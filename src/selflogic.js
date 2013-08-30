b1e9c013b417ab84ccdb76b1f9ce999702effde0
aboynejames <james@aboynejames.co.uk>
2013-08-30 16:36:10 +0100

    press me button implemented

---
+++ src/selflogic.js
@@ -0,0 +1,37 @@
+/**
+* SelfEngine
+*
+* Framework Logic controls
+* @class selfLogic
+*
+* @package    LifestyleLinking part of open sport project
+* @copyright  Copyright (c) 2012 James Littlejohn
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* @version    $Id$
+*/
+var selfLogic = function() {
+	this.status = 'default';
+};
+
+/**
+* Take in clicks/input intentions
+* @method frameworklogic		
+*
+*/	
+selfLogic.prototype.frameworklogic = function(intentionin) {
+//console.log(intentionin);
+	idclick = $(intentionin).attr("id");
+	
+	switch(idclick){
+
+	case "identity": 
+	// make live section
+	$("#contextflow").show();
+	$("#identity").css('background', '#009900');
+	break;
+
+	}
+
+		
+};
+
--- src/start.js
+++ src/start.js
@@ -3,6 +3,14 @@
 * 
 */	
 $(document).ready(function(){
-	
+		
+	liveLogic = new selfLogic();
+			
+		$("a").click(function(e) {
+			e.preventDefault(e);
+			liveLogic.frameworklogic(this);
+			
+			
+		});
 	
 });
