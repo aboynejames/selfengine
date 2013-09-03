a3dc526e2618f3dd8c7a8bf893df77a71c9e5a3a
aboynejames <james@aboynejames.co.uk>
2013-09-03 11:39:59 +0100

    live chart number test

--- test/livedata-chart-test.js
+++ test/livedata-chart-test.js
@@ -7,10 +7,22 @@ casper.test.comment("Scenario: Viewing of a past and future chart");
 
 casper.start(baseUrl, function() {
 	this.test.comment('check placement div for past and future canvas chart');
-		casper.test.assertExists('#pastchart', 'the element exists');
-		casper.test.assertExists('#futurechart', 'the element exists');
+	casper.test.assertExists('#pastchart', 'the element exists');
+	casper.test.assertExists('#futurechart', 'the element exists');
 
 });
+
+casper.then(function() {
+	this.test.comment('the flotr2 canvas class is present');
+	casper.test.assertExists('.flotr-canvas', 'the element exists');
+	
+	this.noofchartslive = this.getElementsAttribute('.flotr-canvas').length;
+this.echo(this.noofchartslive);
+console.log(this.noofchartslive + 'number');
+	casper.test.assertEquals(this.noofchartslive, "2", "the number of charts is correct");
+	
+});
+
 
 casper.run(function() {
     this.test.done(); // I must be called once all the async stuff has been executed
