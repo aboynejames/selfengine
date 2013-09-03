37a62a6ef4140bc420ab4dfc617bd45b34ee0141
aboynejames <james@aboynejames.co.uk>
2013-09-03 12:08:13 +0100

    flotr2 added two charts funcational

--- src/start.js
+++ src/start.js
@@ -12,5 +12,57 @@ $(document).ready(function(){
 			
 			
 		});
+		
+	var container = "pastchart";	
+	var d1 = [[0, 3],[4, 8],[8, 5],[9, 13] ];
+	(function basic(container, d1) {
+
+//console.log(container);
+						// Draw Graph
+						graph = Flotr.draw(container, [d1], {
+								xaxis: {
+										//majorTickFreq: 1
+									mode: 'time',
+									labelsAngle: 45
+								},
+								grid: {
+										//minorVerticalLines: true
+								},
+								yaxis: {
+									min: 0
+								},
+								title: 'Current Times'
+						});
+						
+//console.log(graph);		
+		
+	})(document.getElementById(container), d1);
+
+		var fcontainer = "futurechart";	
+	var f1 = [[0, 3],[4, 8],[8, 5],[9, 13] ];
+	(function basic(container, d1) {
+
+//console.log(container);
+						// Draw Graph
+						graph = Flotr.draw(container, [f1], {
+								xaxis: {
+										//majorTickFreq: 1
+									mode: 'time',
+									labelsAngle: 45
+								},
+								grid: {
+										//minorVerticalLines: true
+								},
+								yaxis: {
+									min: 0
+								},
+								title: 'Current Times'
+						});
+						
+//console.log(graph);		
+		
+	})(document.getElementById(fcontainer), f1);
 	
+
+		
 });
