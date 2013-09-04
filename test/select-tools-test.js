9b082e1b53360dca430609290134f61a5ff26a64
aboynejames <james@aboynejames.co.uk>
2013-09-04 13:45:30 +0100

    select recordtime tests

---
+++ test/select-tools-test.js
@@ -0,0 +1,31 @@
+/*
+* select record time entry
+*/
+var baseUrl = casper.cli.get('baseUrl');
+
+casper.test.comment("Scenario: Select record time entry and enter data");
+
+casper.start(baseUrl, function() {
+	this.test.comment('click on the tools link');
+	this.mouseEvent('click', '#tools');
+
+});
+
+casper.then(function() {
+	this.test.comment('click on the swim record link');
+	casper.test.assertExists('#recordtime');
+		
+});
+
+casper.then(function() {
+	this.test.comment('click on the swim record link');
+	casper.test.assertExists('#recordtime', 'the element exists');
+	
+});
+
+
+casper.run(function() {
+this.echo(this.getHTML());
+    this.test.done(); // I must be called once all the async stuff has been executed
+
+});
