468d70558804c22e3497c7ae7d6d9e2d9553190e
James Littlejohn <james@aboynejames.co.uk>
2014-10-14 16:53:59 +0100

    documenation update

---
+++ out/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "datamodel",
+        "livedata",
+        "pouchdbSettings",
+        "selfLogic",
+        "selfprediction",
+        "viewtemplates"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
